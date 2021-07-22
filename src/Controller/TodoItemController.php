f3496e3f4c815dea551cb6207620d9016ddf6b15
Tautvydas Dulskis <tautvydas.dulskis@hostinger.com>
2021-07-22 21:11:35 +0300

    ACL and security for routes. CRUD for Persons, Users

--- src/Controller/TodoItemController.php
+++ src/Controller/TodoItemController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Manager\TodoItemManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 #[Route('/todo/item')]
 class TodoItemController extends AbstractController
---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Person;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    {
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $person = new Person();
+        $person->setName('Administratorius');
+        $manager->persist($person);
+
+        $user = new User();
+        $user->setPerson($person);
+        $user->setEmail('admin@admin.lt');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->passwordHasher->hashPassword($user, 'slaptazodis'));
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
